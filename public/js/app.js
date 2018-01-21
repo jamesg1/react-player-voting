4f2b44a9d00f0baaaffec248d4966c3cf868ba7e
James Gosbell <jamesgosbell@gmail.com>
2018-01-21 23:11:03 +1000

    Added PlayerList and Player React components with voting state

---
+++ public/js/app.js
@@ -0,0 +1,86 @@
+class PlayerList extends React.Component {
+    state = {
+        players: [],
+    };
+
+    componentDidMount() {
+        this.setState({ players: Seed.players });
+    }
+
+    handlePlayerUpVote = (playerId) => {
+        const nextPlayers = this.state.players.map((player) => {
+            if (player.id === playerId) {
+                return Object.assign({}, player, {
+                    votes: player.votes + 1,
+                });
+            } else {
+                return player;
+            }
+        });
+
+        this.setState({
+            players: nextPlayers,
+        })
+    };
+
+    render() {
+        const players = this.state.players.sort((a, b) => (
+            b.votes - a.votes
+        ));
+        const playerComponents = players.map((player) => (
+            <Player
+                key={'player-' + player.id}
+                id={player.id}
+                title={player.title}
+                description={player.description}
+                url={player.url}
+                votes={player.votes}
+                submitterAvatarUrl={player.submitterAvatarUrl}
+                playerImageUrl={player.playerImageUrl}
+                onVote={this.handlePlayerUpVote}
+            />
+        ));
+        return (
+            <div className='ui unstackable items'>
+                {playerComponents}
+            </div>
+        );
+    }
+}
+
+class Player extends React.Component {
+    handleUpVote = () => {
+        this.props.onVote(this.props.id);
+    };
+
+    render() {
+        return (
+            <div className='item'>
+                <div className='image'>
+                    <img src={this.props.playerImageUrl} />
+                </div>
+                <div className='middle aligned content'>
+                    <a onClick={this.handleUpVote}>
+                        <i className='large caret up icon' />
+                        {this.props.votes}
+                    </a>
+                    <div className='description'>
+                        <a href={this.props.url}>
+                            {this.props.title}
+                        </a>
+                        <p>{this.props.description}</p>
+                    </div>
+                    <div className='extra'>
+                        <span>Submitted by:</span>
+                        <img
+                            className='ui avatar image'
+                            src={this.props.submitterAvatarUrl}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<PlayerList />, document.getElementById('content'));
